dbf21559e6088c9bbbf16cf3682da12e3bed5fa5
Herley Antoine <ffast2000@gmail.com>
2024-08-08 21:41:38 -0400

    first commit

---
+++ isolate-speaker.ts
@@ -0,0 +1,69 @@
+import fs from "fs";
+import path from "path";
+import { exec } from "child_process";
+
+interface SegmentProps {
+  text: string;
+  start: number;
+  end: number;
+  speaker: string;
+}
+
+interface IsolateSpeakersProps {
+  transcription: SegmentProps[];
+  audioFilePath: string;
+}
+
+interface ExtractedSegment {
+  start: number;
+  end: number;
+  filePath: string;
+}
+
+interface SpeakerSegments {
+  [speaker: string]: ExtractedSegment[];
+}
+
+export async function isolateSpeakers({
+  transcription,
+  audioFilePath,
+}: IsolateSpeakersProps): Promise<SpeakerSegments> {
+  const speakers: SpeakerSegments = {};
+
+  transcription.forEach((segment: SegmentProps) => {
+    const { speaker, start, end } = segment;
+    if (!speakers[speaker]) {
+      speakers[speaker] = [];
+    }
+    speakers[speaker].push({ start, end, filePath: "" });
+  });
+
+  const extractionPromises = Object.keys(speakers).map((speaker) => {
+    const speakerSegments = speakers[speaker];
+    return Promise.all(
+      speakerSegments.map((segment, index) => {
+        return new Promise<void>((resolve, reject) => {
+          const outputFilePath = path.join(
+            process.cwd(),
+            `speaker_${speaker}_segment_${index}.mp3`
+          );
+          const command = `ffmpeg -i ${audioFilePath} -ss ${
+            segment.start / 1000
+          } -to ${segment.end / 1000} -c copy ${outputFilePath}`;
+
+          exec(command, (error, stdout, stderr) => {
+            if (error) {
+              reject(`Error extracting segment: ${error.message}`);
+            } else {
+              console.log(`Extracted segment to: ${outputFilePath}`);
+              segment.filePath = outputFilePath;
+              resolve();
+            }
+          });
+        });
+      })
+    );
+  });
+
+  return Promise.all(extractionPromises).then(() => speakers);
+}
---
+++ server.ts
@@ -0,0 +1,64 @@
+import { isolateSpeakers } from "./isolate-speaker";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const { exec } = require("child_process");
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json());
+
+app.get("/api", (req: any, res: any) => {
+  res.send("Welcome to the first api for audio manipulation");
+});
+
+app.post("/api/isolate-speakers", async (req: any, res: any) => {
+  const { transcription, audioFilePath } = req.body;
+
+  if (!transcription || !audioFilePath) {
+    return res
+      .status(400)
+      .json({ error: "Transcription and audioFilePath are required." });
+  }
+
+  try {
+    const speakers = await isolateSpeakers({ transcription, audioFilePath });
+    res.json(speakers);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/combine-audio", (req: any, res: any) => {
+  const { newAudioFilePath, backgroundMusicFilePath, outputFilePath } =
+    req.body;
+
+  console.log("Received file paths:");
+  console.log("newAudioFilePath:", newAudioFilePath);
+  console.log("backgroundMusicFilePath:", backgroundMusicFilePath);
+  console.log("outputFilePath:", outputFilePath);
+
+  if (!newAudioFilePath || !backgroundMusicFilePath || !outputFilePath) {
+    return res
+      .status(400)
+      .json({ error: "Missing required file paths in the request body" });
+  }
+
+  const ffmpegCommand = `ffmpeg -i "${newAudioFilePath}" -i "${backgroundMusicFilePath}" -filter_complex "[0:a][1:a]amerge=inputs=2[a]" -map "[a]" -ac 2 "${outputFilePath}"`;
+
+  exec(ffmpegCommand, (error: any, stdout: any, stderr: any) => {
+    if (error) {
+      console.error("Error executing ffmpeg:", error.message);
+      console.error("stderr:", stderr);
+      return res
+        .status(500)
+        .json({ error: `Error combining audio: ${stderr}` });
+    }
+    console.log("stdout:", stdout);
+    res.json({ message: "Audio files combined successfully", outputFilePath });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
