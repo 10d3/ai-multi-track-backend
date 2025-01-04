42667148777d5c6acda5116ce8783511de25b74c
Herley Antoine <ffast2000@gmail.com>
2025-01-03 20:35:30 -0500

    adding: redis

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -52,8 +52,8 @@ const audioProcessingQueue = new Queue("audio-processing", {
   connection: {
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
+    // username: process.env.REDIS_USERNAME,
+    // password: process.env.REDIS_PASSWORD,
   },
 });
 
--- src/utils/worker.ts
+++ src/utils/worker.ts
@@ -403,8 +403,8 @@ const worker = new Worker<JobData>(
     connection: {
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
-      username:process.env.REDIS_USERNAME,
-      password: process.env.REDIS_PASSWORD
+      // username:process.env.REDIS_USERNAME,
+      // password: process.env.REDIS_PASSWORD
     },
     concurrency: 5,
   }
