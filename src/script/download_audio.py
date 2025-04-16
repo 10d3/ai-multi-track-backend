7838db5aceceef6443e0eb552f842c3992ffd1c9
Herley Antoine <ffast2000@gmail.com>
2025-04-15 22:09:23 -0400

    chore: update pytubefix dependency and enable po token usage
    Updated pytubefix from version 8.1.1 to 8.12.3 in requirements.txt to ensure compatibility and access to the latest features. Additionally, modified the download_audio.py script to enable the use of po token for improved functionality.

--- src/script/download_audio.py
+++ src/script/download_audio.py
@@ -5,7 +5,7 @@ from pytubefix.cli import on_progress
 url = sys.argv[1]
 output_filename = sys.argv[2]
 
-yt = YouTube(url)
+yt = YouTube(url, use_po_token = true)
 
 # Get audio stream
 audio_stream = yt.streams.filter(only_audio=True).first()
